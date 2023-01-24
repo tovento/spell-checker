b05d83c88c825c62ef485f4207269d7779459858
tovento <92781588+tovento@users.noreply.github.com>
2023-01-24 10:00:57 +0200

    Use coverage

--- tasks.py
+++ tasks.py
@@ -7,3 +7,11 @@ def pylint(ctx):
 @task
 def test(ctx):
     ctx.run("pytest src/tests", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)

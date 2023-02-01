ac8db7eb100d44ed378e63ae74bd7a5e3189c1ce
tovento <92781588+tovento@users.noreply.github.com>
2023-02-01 10:11:22 +0200

    Write tests for DL algorithm

---
+++ src/tests/algorithms/damerau_levenshtein_test.py
@@ -0,0 +1,34 @@
+import unittest
+from algorithms.damerau_levenshtein import DamerauLevenshtein
+
+class TestDamerauLevenshtein(unittest.TestCase):
+    def setUp(self):
+        self.dl = DamerauLevenshtein()
+
+    def test_addition(self):
+        word1 = "cat"
+        word2 = "catnip"
+        distance = self.dl.calculate_distance(word1, word2)
+
+        self.assertEqual(distance, 3)
+
+    def test_deletion(self):
+        word1 = "doghill"
+        word2 = "dghll"
+        distance = self.dl.calculate_distance(word1, word2)
+
+        self.assertEqual(distance, 2)
+
+    def test_substitution(self):
+        word1 = "moon"
+        word2 = "noon"
+        distance = self.dl.calculate_distance(word1, word2)
+
+        self.assertEqual(distance, 1)
+
+    def test_transposition(self):
+        word1 = "algorithm"
+        word2 = "lagorithm"
+        distance = self.dl.calculate_distance(word1, word2)
+
+        self.assertEqual(distance, 1) 

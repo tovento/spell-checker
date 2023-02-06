c28276fa160dc78407d8d08eaf0dd84bad6b6b4f
tovento <92781588+tovento@users.noreply.github.com>
2023-02-06 09:05:03 +0200

    Clean up algorithm code

--- src/algorithms/damerau_levenshtein.py
+++ src/algorithms/damerau_levenshtein.py
@@ -16,11 +16,7 @@ class DamerauLevenshtein():
         len1 = len(word1)
         len2 = len(word2)
 
-        maxdist = len1 + len2
-
-        matrix = [[maxdist for _ in range(len2+2)]]
-        matrix += [[maxdist] + list(range(len2 + 1))]
-        matrix += [[maxdist, x] + [0] * len2 for x in range(1, len1 + 1)]
+        matrix = self.create_matrix(len1, len2)
 
         last_row_with_letter = {}
 
@@ -43,12 +39,17 @@ class DamerauLevenshtein():
                     substitution_cost = 1
 
                 matrix[row+1][column+1] = min(
-                        matrix[row][column] + substitution_cost,
-                        matrix[row+1][column] + 1,
-                        matrix[row][column+1] + 1,
-                        matrix[last_matching_row][last_matching_column]
-                            + (row - last_matching_row - 1) + 1
-                            + (column - last_matching_column - 1))
+                                            self.calculate_substitution(
+                                                matrix, row, column,
+                                                substitution_cost),
+                                            self.calculate_addition(
+                                                matrix, row, column),
+                                            self.calculate_deletion(
+                                                matrix, row, column),
+                                            self.calculate_transposition(
+                                                matrix, row, column,
+                                                last_matching_row,
+                                                last_matching_column))
 
                 if substitution_cost == 0:
                     last_matching_column = column
@@ -56,3 +57,34 @@ class DamerauLevenshtein():
             last_row_with_letter[word1_char] = row
 
         return matrix[-1][-1]
+
+    def create_matrix(self, len1, len2):
+        """Creates a matrix the size of (len1 + 2) * (len2 + 2).
+        """
+        maxdist = len1 + len2
+
+        matrix = [[maxdist for _ in range(len2+2)]]
+        matrix += [[maxdist] + list(range(len2 + 1))]
+        matrix += [[maxdist, x] + [0] * len2 for x in range(1, len1 + 1)]
+
+        return matrix
+
+    def calculate_substitution(self, matrix: list, row: int, column: int, substitution_cost: int):
+        """TODO """
+        return matrix[row][column] + substitution_cost
+
+    def calculate_addition(self, matrix: list, row: int, column: int):
+        """TODO """
+        return matrix[row+1][column] + 1
+
+    def calculate_deletion(self, matrix: list, row: int, column: int):
+        """TODO """
+        return matrix[row][column+1] + 1
+
+    def calculate_transposition(self, matrix: list, row: int,
+                                column: int, last_matching_row: int,
+                                last_matching_column: int):
+        """TODO """
+        return (matrix[last_matching_row][last_matching_column]
+                + (row - last_matching_row - 1) + 1
+                + (column - last_matching_column -1))
